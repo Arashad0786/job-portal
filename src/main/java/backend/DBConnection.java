76f66ccca59cabdbc3df4a7b4001494620a7d2e7
Arashad0786 <tahiralimahason@gmail.com>
2026-01-11 13:22:14 +0530

    Update database connection for Raileawy

--- src/main/java/backend/DBConnection.java
+++ src/main/java/backend/DBConnection.java
@@ -15,8 +15,10 @@ public class DBConnection {
    static  Connection con;
     static {
         try{
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//             con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal","root","44@@");
             Class.forName("com.mysql.cj.jdbc.Driver");
-             con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal","root","44@@");
+             con=DriverManager.getConnection("mysql://root:dRtSzHvDsuQLkrHRXRDRbQwnCZgPQHMK@mysql.railway.internal:3306/railway","root","dRtSzHvDsuQLkrHRXRDRbQwnCZgPQHMK");
         }
         catch(Exception e){
             System.out.print(e);
