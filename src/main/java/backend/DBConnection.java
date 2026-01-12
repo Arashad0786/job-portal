a33a5ccddcc6ae9a197b30d0cc17c108bbbc47f6
Arashad0786 <tahiralimahason@gmail.com>
2026-01-12 12:29:03 +0530

    Correct Database connection

--- src/main/java/backend/DBConnection.java
+++ src/main/java/backend/DBConnection.java
@@ -6,6 +6,7 @@ package backend;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -15,19 +16,19 @@ public class DBConnection {
    static  Connection con;
     static {
         try{
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//             con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal","root","44@@");
             Class.forName("com.mysql.cj.jdbc.Driver");
-<<<<<<< HEAD
-             con=DriverManager.getConnection("mysql://root:dRtSzHvDsuQLkrHRXRDRbQwnCZgPQHMK@mysql.railway.internal:3306/railway","root","dRtSzHvDsuQLkrHRXRDRbQwnCZgPQHMK");
-=======
-             con=DriverManager.getConnection("jdbc:mysql://containers-us-west-XX.railway.app:6543/jobportal","root","44@@");
->>>>>>> 525e107e60b3f84d5d047a63b9a253591a19e066
+             con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal","root","44@@");
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//             con=DriverManager.getConnection("jdbc:mysql://root:dRtSzHvDsuQLkrHRXRDRbQwnCZgPQHMK@mysql.railway.internal:3306/railway","root","dRtSzHvDsuQLkrHRXRDRbQwnCZgPQHMK");
+//=======
         }
+           
         catch(Exception e){
             System.out.print(e);
         }
     }
+    
     public  static  Connection getConnect(){
         return con;
     }
